fd8b0a9134de422bce66cbb8cb521226e93b69ac
subhodynamics <subhadeeppathak2013@gmail.com>
2025-04-30 14:24:56 +0530

    https

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://ec2-65-1-135-132.ap-south-1.compute.amazonaws.com:3000", // Correct backend URL
+  baseURL: "https://ec2-65-1-135-132.ap-south-1.compute.amazonaws.com:3000",
   headers: {
     "Content-Type": "application/json",
   },
